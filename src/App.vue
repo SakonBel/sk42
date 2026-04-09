18520ebf9e1f41eb7cfd007bdf31226fb3e69192
Sakon Plangklang <iamsabell@gmail.com>
2026-04-09 23:28:47 +0700

    update inventory 09/04/2026

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import location from "./assets/location.json";
 import price from "./assets/price.json";
 import promotion from "./assets/promotion.json";
 import promotion2 from "./assets/promotion2.json";
-import promotion3 from "./assets/promotion3.json";
+// import promotion3 from "./assets/promotion3.json";
 
 import datatable from "./components/DataTable.vue";
 import singleitem from "./components/SingleItem.vue";
@@ -161,18 +161,18 @@ export default {
       });
 
       // Add additional promotion 3
-      promotion3.forEach((proPrice) => {
-        items.forEach((item) => {
-          if (proPrice["No."] === item.name) {
-            item.disc = "สูงสุด 50%";
-            item.sale = "ซื้อ 1 แถม 1";
-            item.isOnPromotion = true;
-            item.isOnStackPromotion = false;
-            item.proName = "Buy 1 Get 1";
-            item.proCat = "BOGO";
-          }
-        });
-      });
+      // promotion3.forEach((proPrice) => {
+      //   items.forEach((item) => {
+      //     if (proPrice["No."] === item.name) {
+      //       item.disc = "สูงสุด 50%";
+      //       item.sale = "ซื้อ 1 แถม 1";
+      //       item.isOnPromotion = true;
+      //       item.isOnStackPromotion = false;
+      //       item.proName = "Buy 1 Get 1";
+      //       item.proCat = "BOGO";
+      //     }
+      //   });
+      // });
 
       // Add sizes to items appropriately
       items.forEach((item, index) => {
